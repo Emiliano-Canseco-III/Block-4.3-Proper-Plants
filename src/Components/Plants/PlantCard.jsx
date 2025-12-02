640f4e36e44a42bf282a4b3da6f106e8db46e81e
Milo Canseco <canseco3rd@gmail.com>
2025-12-01 22:23:56 -0700

    Fixed bug that wouldn't show plant images.

--- src/Components/Plants/PlantCard.jsx
+++ src/Components/Plants/PlantCard.jsx
@@ -1,12 +1,12 @@
 function PlantCard({ plant, onAddToCart }) {
-  const { name, emoji } = plant;
+  const { name, image } = plant;
 
   // The button logic is simple: always call onAddToCart with the plant object.
   // The logic inside App.jsx handles whether to add or increment.
 
   return (
     <div className="plant-card">
-      <div className="plant-emoji">{emoji}</div>
+      <div className="plant-emoji">{image}</div>
       <h3>{name}</h3>
       <button onClick={() => onAddToCart(plant)}>Add to cart</button>
     </div>
